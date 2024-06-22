13f4c0fb2a9bc54c6ec31992ec4940c268dcedde
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-22 12:15:49 +0700

    js lab 131

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+// จงเรียงข้อมูลใน arr โดยเรียงตามค่า element ยกกำลัง 2 จากน้อยไปหามาก
+
+const arr = [-3, 2, 0, -7, 4, 6];
+const arr2 = arr.map(arr => Math.pow(arr, 2))
+const sortArr2 = arr2.sort((a, b) => a - b)
+console.log(sortArr2)
